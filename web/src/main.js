a4033293f111125aa1f2fbbfd930d07b4e37c5e6
xie <742152637@qq.com>
2020-11-17 13:43:35 +0800

    重新使用vue cli4创建项目

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@babel/polyfill"; // IE 兼容
+
+// 挂载环境变量中的配置信息，然后在js中使用$src 访问。
+Vue.prototype.$src = process.env.VUE_APP_PUBLIC_PATH;
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ web/vue.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
+
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+const webpack = require("webpack");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
+
+module.exports = {
+  publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "./", // 默认'/'，部署应用包时的基本 URL
+  // outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
+  // assetsDir: "", // 相对于outputDir的静态资源(js、css、img、fonts)目录
+  lintOnSave: true,
+  runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
+  productionSourceMap: !IS_PROD, // 生产环境的 source map
+  parallel: require("os").cpus().length > 1,
+  pwa: {},
+  chainWebpack: config => {
+    // 添加别名
+    config.resolve.alias
+      .set("vue$", "vue/dist/vue.esm.js")
+      .set("@", resolve("src"))
+      .set("@assets", resolve("src/assets"))
+      .set("@scss", resolve("src/assets/scss"))
+      .set("@components", resolve("src/components"))
+      .set("@plugins", resolve("src/plugins"))
+      .set("@views", resolve("src/views"))
+      .set("@router", resolve("src/router"))
+      .set("@store", resolve("src/store"))
+      .set("@layouts", resolve("src/layouts"))
+      .set("@static", resolve("src/static"));
+
+    // 修复 HMR(热更新)失效
+    config.resolve.symlinks(true);
+
+    // 删除 moment 除 zh-cn 中文包外的其它语言包，无需在代码中手动引入 zh-cn 语言包
+    config
+      .plugin("ignore")
+      .use(
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn$/)
+      );
+
+    return config;
+  },
+  devServer: {
+    // overlay: { // 让浏览器 overlay 同时显示警告和错误
+    //   warnings: true,
+    //   errors: true
+    // },
+    open: true, // 是否打开浏览器
+    host: "localhost",
+    port: "8060", // 代理断就
+    // https: false,
+    hotOnly: true, // 热更新
+    proxy: {
+      "/api": {
+        target:
+          "http://localhost:8070", // 目标代理接口地址
+        secure: false,
+        changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
+        // ws: true, // 是否启用websockets
+        pathRewrite: {
+          "^/api": "/"
+        }
+      }
+    }
+  },
+  css: {
+    extract: IS_PROD,
+    sourceMap: false,
+    loaderOptions: {
+      scss: {
+        // 向全局sass样式传入共享的全局变量, $src可以配置图片cdn前缀
+        // 详情: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
+        prependData: `
+        @import "@scss/variables.scss";
+        @import "@scss/mixins.scss";
+        @import "@scss/function.scss";
+        $src: "${process.env.VUE_APP_OSS_SRC}";
+        `
+      }
+    }
+  }
+};
